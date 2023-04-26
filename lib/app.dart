930477f4de339cabd55596c6f1c369595ab25f4a
bhupeshbc <bhupeshbc12@gmail.com>
2023-04-26 11:19:32 +0545

    route

--- lib/app.dart
+++ lib/app.dart
@@ -3,33 +3,29 @@ import 'package:new_app/view/Area_view.dart';
 
 import 'view/arithematic_view.dart';
 import 'view/dashboard.dart';
-import 'view/rich_text.dart';
+import 'view/rich_view.dart';
 import 'view/simple_view.dart';
 
-
-class App
- extends StatelessWidget {
-  const App
-  ({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      // home:    
+      // home:
       //   Dashboard(),
       // ArithmeticView(),
       // SimpleInterest(),
 
       initialRoute: '/',
       routes: {
-        '/': (context)=> const Dashboard(),
-        '/arithmeticRoute' :(context) => const ArithmeticView(),
-        '/siRoute' :(context) =>  const SimpleInterest(),
-        '/radiusRoute' :(context) =>  const CircleView(),
-        '/richTextRoute' :(context) =>  const RichTextView(),
+        '/': (context) => const Dashboard(),
+        '/arithmeticRoute': (context) => const ArithmeticView(),
+        '/siRoute': (context) => const SimpleInterest(),
+        '/radiusRoute': (context) => const CircleView(),
+        '/richTextRoute': (context) => const RichTextView(),
       },
-   
     );
   }
 }
